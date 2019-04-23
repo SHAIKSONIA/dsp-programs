df343e83c93001adcb70011192b5d3f31ff2bc2f
SHAIKSONIA <47148729+SHAIKSONIA@users.noreply.github.com>
2019-04-23 18:21:43 +0530

    Add files via upload

---
+++ arithmetic operations.py
@@ -0,0 +1,10 @@
+a=input("enter a number")
+b=input("enter a number")
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print "addition of the two numbers is",c
+print "subtraction of the two numbers is", d
+print "multiplication of the two numbers is", e
+print  "division of the two numbers is", f
